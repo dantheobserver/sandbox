8fa5d22f184d3f77014760b4a653fdb263a6c19b
dantheobserver <daniel.alan.p@gmail.com>
2018-02-24 09:07:54 -0800

    Added tests and optimized rotation algo

--- src/sandbox/tetromino_drop.clj
+++ src/sandbox/tetromino_drop.clj
@@ -1,74 +1,56 @@
 (ns sandbox.tetromino-drop
-  (:require [clojure.math.numeric-tower :as math]))
+  (:require [clojure.string :as str]))
 
-(def tetrominos {:I {:pieces [[1 0] [1 1] [1 2] [1 3]]
-                     :pivot [1 2.5]}
-                 :L {:pieces [[1 0] [1 1] [1 2] [2 2]]
-                     :pivot [0 1]}
-                 :O {:pieces [[0 0] [0 1] [1 0] [1 1]]
+(def tetrominos {:I {:pieces #{[1 0] [1 1] [1 2] [1 3]}
                      :pivot [1.5 1.5]}
-                 :Z {:pieces [[0 0] [1 0] [1 1] [2 1]]
-                     :pivot [1 1]
-                     }})
+                 :L {:pieces #{[1 0] [1 1] [1 2] [2 2]}
+                     :pivot [1 1]}
+                 :O {:pieces #{[0 0] [0 1] [1 0] [1 1]}
+                     :pivot [0.5 0.5]}
+                 :Z {:pieces #{[0 0] [1 0] [1 1] [2 1]}
+                     :pivot [1 1]}})
 
-
-(defn- abs [x] (max x (- x)))
-
-(defn coord->rel
+(defn- coord->rel
   "Finds the cell's position relative to the pivot"
   [cell pivot]
   (vec (map - cell pivot)))
 
-(defn rel->coord
+(defn- rel->coord
   "Finds the cell's coordinate given the pivot"
   [rel pivot]
   (vec (map + rel pivot)))
 
-;; Cover immediate neighbor cardinals first
-(defn next-diag-pos-cw
-  [[rx ry :as rel-pos]]
-  (if (reduce = rel-pos)
-    [(- rx) ry]
-    [rx (- ry)]))
-
-(defn next-card-pos-cw
-  [[rx ry]]
-  (let [swapped (vector ry rx)]
-    (if (= (abs ry) 1)
-      (map - swapped)
-      swapped)))
+(defn- next-pos-cw [[rx ry]] [(- ry) rx]) ;; It was that simple!?
 
 (defn next-coord-cw
-  "get next pivot"
-  [cell pivot]
-  (let [rel-pos (coord->rel cell pivot)
-        diag? (reduce = (map abs rel-pos))
-        next-pos (if diag?
-                   (next-diag-pos-cw rel-pos)
-                   (next-card-pos-cw rel-pos))]
-    ;; (println next-pos)
-    (rel->coord next-pos pivot)))
+  "Calculate the next rotation"
+  [pivot cell]
+  (-> cell
+      (coord->rel pivot)
+      next-pos-cw
+      (rel->coord pivot)))
 
-#_(next-coord-cw [0 0] [1 1]) ;; NW
-#_(next-coord-cw [2 0] [1 1]) ;; NE
-#_(next-coord-cw [2 2] [1 1]) ;; SE
-#_(next-coord-cw [0 2] [1 1]) ;; SW
-#_(next-coord-cw [1 0] [1 1]) ;; N
-#_(next-coord-cw [2 1] [1 1]) ;; E
-#_(next-coord-cw [1 2] [1 1]) ;; S
-#_(next-coord-cw [0 1] [1 1]) ;; W
+(defn rotate-piece
+  [{:keys [pivot pieces] :as piece}]
+  (let [next-coord (into #{} (map #(next-coord-cw pivot %)) pieces)]
+    (->> pieces
+         (map #(next-coord-cw pivot %))
+         (into #{})
+         (assoc piece :pieces))))
 
-#_(is (= 1 1) true)
-(defn get-piece
-  ([piece] (get-piece piece identity))
-  ([piece translate] (translate (piece tetrominos))))
+(defn- get-bound [pieces]
+  (apply max (map #(apply max %) pieces)))
 
-(defn boundary [[pieces]]
-  (reduce ))
-(defn rotate
-  [[pieces pivot]]
-  )
-
-(defn rotate-piece-cw [piece])
-
-(get-piece :I)
+(defn print-piece
+  "Pretty prints a piece on screen with specified size."
+  [{:keys [pieces]} size]
+  (let [bound (inc (get-bound pieces))
+        grid-range (range bound)]
+    (->> (for [y grid-range
+               x grid-range]
+           (if (pieces [x y]) "*" "."))
+         (map #(str/join (repeat size %)))
+         (partition bound)
+         (mapcat #(repeat size %))
+         (map str/join)
+         (str/join "\n"))))
